4c3b7dcfca5958a8236a19a84a97420c75550f16
LAPTOP-7KBHEOPS\Gun <gunsuebtrangan@gmail.com>
2020-09-12 13:41:51 +0700

    uuu

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+
+  const onInputChange = (e) => {
+    setTodoInput(e.currentTarget.value);
+  };
+
+  const addTodo = () => {
+    var newTodos = JSON.parse(JSON.stringify(todos));
+    newTodos.push({
+      id: todos.length,
+      title: todoInput,
+      isDone: false,
+    });
+
+    setTodos(newTodos);
+    setTodoInput("");
+  };
+
+  const updateIsDone = (todo) => {
+    var newTodos = JSON.parse(JSON.stringify(todos));
+    for (var i = 0; i < newTodos.length; i++) {
+      if (newTodos[i].id === todo.id) {
+        newTodos[i].isDone = !newTodos[i].isDone;
+      }
+    }
+
+    setTodos(newTodos);
+  };
+
+  var todoElements = [];
+  for (var i = 0; i < todos.length; i++) {
+    const todo = todos[i];
+    todoElements.push(
+      <div
+        key={i}
+        style={{ textDecorationLine: todo.isDone ? "line-through" : "" }}
+        onClick={() => updateIsDone(todo)}
+      >
+        {todo.title}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>todo list</h1>
+      {todoElements}
+      {/* {todos.map((todo, index) => {
+        return <div key={index}>{todo}</div>
+      })} */}
+      <div>
+        <input type="text" onChange={onInputChange} value={todoInput}></input>{" "}
+        <button onClick={addTodo}>Add</button>
+      </div>
     </div>
   );
 }
